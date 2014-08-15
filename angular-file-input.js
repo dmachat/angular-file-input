e0b3aa97e8b6200d8d6c1b618d0bae1b3028b005
Drew Machat <drew@alleyinteractive.com>
2014-08-14 21:43:25 -0400

    add file reader service

--- angular-file-input.js
+++ angular-file-input.js
@@ -9,7 +9,7 @@ angular.module('fileInput').directive('fileInputButton', function () {
       var el = angular.element(element);
       var fileInput = angular.element('<input type="file" />');
       el.append(fileInput);
-      fileInput.on('change', function uploadButtonFileInputChange() {
+      fileInput.on('change', function fileInputButtonChange() {
         if (fileInput[0].files && fileInput[0].files.length === 0) {
           return;
         }
@@ -20,3 +20,46 @@ angular.module('fileInput').directive('fileInputButton', function () {
     }
   };
 });
+'use strict';
+var fileReader = function ($q) {
+  var onLoad = function (reader, deferred, scope) {
+    return function () {
+      scope.$apply(function () {
+        deferred.resolve(reader.result);
+      });
+    };
+  };
+  var onError = function (reader, deferred, scope) {
+    return function () {
+      scope.$apply(function () {
+        deferred.reject(reader.result);
+      });
+    };
+  };
+  var onProgress = function (reader, scope) {
+    return function (event) {
+      scope.$broadcast('fileProgress', {
+        total: event.total,
+        loaded: event.loaded
+      });
+    };
+  };
+  var getReader = function (deferred, scope) {
+    var reader = new FileReader();
+    reader.onload = onLoad(reader, deferred, scope);
+    reader.onerror = onError(reader, deferred, scope);
+    reader.onprogress = onProgress(reader, scope);
+    return reader;
+  };
+  var readAsDataURL = function (file, scope) {
+    var deferred = $q.defer();
+    var reader = getReader(deferred, scope);
+    reader.readAsDataURL(file);
+    return deferred.promise;
+  };
+  return { readAsDataUrl: readAsDataURL };
+};
+angular.module('fileInput').factory('fileReader', [
+  '$q',
+  fileReader
+]);
